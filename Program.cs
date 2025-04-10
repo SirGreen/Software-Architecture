578c482dca9c9017d1e43e216ec1ca8b04445643
Tran Thi Hong Hanh <tthh260904@gmail.com>
2025-04-10 16:42:38 +0700

    Initial Patient

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
+using BTL_SA.Infrastructure;
+using BTL_SA.Modules.StaffMana.Infrastructure;
+using BTL_SA.Modules.StaffMana.Application;
+using BTL_SA.Modules.StaffMana.Facade;
+
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+// builder.Services.AddControllersWithViews();
+// builder.Services.AddSingleton<DatabaseService>();
+// builder.Services.AddSingleton<IStaffRepository, StaffRepositoryDb>();
+// builder.Services.AddSingleton<ICredentialRepository, CredentialRepositoryDb>();
+
+// builder.Services.AddSingleton<IQuerySevice, QueryService>();
+// builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
+// builder.Services.AddSingleton<IAssignmentService, AssignmentService>();
+// builder.Services.AddSingleton<ICredentialService, CredentialService>();
+
+// builder.Services.AddSingleton<FacadeStaffInformationManagement>();
+// Add Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "BTL-SA API",
+        Version = "v1",
+        Description = "API documentation for the BTL-SA application"
+    });
+});
 
 var app = builder.Build();
 
@@ -9,8 +36,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Enable Swagger in development mode
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BTL-SA API v1");
+        c.RoutePrefix = "swagger"; // Swagger UI will be available at /swagger
+    });
 }
 
 app.UseHttpsRedirection();
